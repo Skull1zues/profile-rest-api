f2c7804679452d1f28f8b9eb5042a07cb8eb369d
Soumyadip Barik <soumyadip.barik@vodafone.com>
2024-08-07 01:47:04 +0530

    Add signin api-TokenAuthentication

--- profile_api/urls.py
+++ profile_api/urls.py
@@ -8,7 +8,9 @@ router=DefaultRouter()
 router.register('hello-viewset',views.HelloViewSet , base_name='hello-viewset')
 router.register('profiles',views.UserProfileViewSet, base_name='profiles')
 
+
 urlpatterns = [
     path('hello-view/',views.HelloApiView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
     path('',include(router.urls))   
 ]
--- profile_api/views.py
+++ profile_api/views.py
@@ -6,6 +6,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 from profile_api import serializer
@@ -104,5 +106,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permission.UpdateOwnProfile,)
     filter_backends =(filters.SearchFilter,)
     search_fields = ('name','email',)
+    
+    
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
+    
     
     
